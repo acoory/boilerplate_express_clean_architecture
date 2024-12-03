4170e0d62749721fc6f1db75e386dbd92d2a942f
Anthony Cory <acory@digital-paca.fr>
2024-12-03 17:18:07 +0100

    Add verif is email is valid

--- src/domain/services/userService.ts
+++ src/domain/services/userService.ts
@@ -1,6 +1,5 @@
-// src/domain/services/userService.ts
 import UserRepository from '../repositories/userRepository';
-import UserModel, {UserModelAttributes} from "../models/user.model";
+import {UserModelAttributes} from "../models/user.model";
 
 const bcrypt = require('bcrypt');
 
@@ -11,15 +10,20 @@ class UserService {
             throw new Error('Email and password are required');
         }
 
-        try {
+        if (!this.verifEmail(email)) {
+            throw new Error('Invalid email');
+        }
 
+        try {
             const userExist = await UserRepository.getUserByEmail(email);
 
             if (userExist) {
                 throw new Error('User already exists');
             }
 
-            const user = await UserRepository.createUser(email, password);
+            const hashedPassword = await bcrypt.hash(password, 10);
+
+            const user = await UserRepository.createUser(email, hashedPassword);
             return user;
         } catch (error: any) {
             throw new Error(error.message);
@@ -60,6 +64,11 @@ class UserService {
         }
 
         return user;
+    }
+
+    static verifEmail(email: string): boolean {
+        const emailRegex = /\S+@\S+\.\S+/;
+        return emailRegex.test(email);
     }
 
 }
